0e899873eb0f738d950deaa55723c0024c5aa9e0
isiagi <isiagigeofrey0@gmail.com>
2023-04-24 11:53:49 +0300

    refactor: Add, modify models, rewire entry file

--- server.ts
+++ server.ts
@@ -1,11 +1,19 @@
 import express, { Express, Request, Response } from "express";
 import booksRouter from './src/routers/booksRouter'
 import ordersRouter from './src/routers/orderRouter'
+import userRouter from './src/routers/userRouter'
 import connectDB from "./src/db/connect";
+import cors from 'cors'
+
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app:Express = express();
 
+app.use(cors());
 app.use(express.json())
+app.use('/api/v1/auth/', userRouter)
 app.use('/api/v1/book/', booksRouter)
 app.use('/api/v1/order/', ordersRouter)
 
---
+++ src/models/userModel.ts
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcryptjs';
+import JWT from 'jsonwebtoken';
+
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const userSchema = new Schema({
+  username: String,
+  password: {
+    type: String,
+    required: [true, "Please input password"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please input a valid email address"],
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide valid email",
+    ],
+  },
+  passwordResetToken: String,
+  resetPasswordExpire: Date,
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (password:string) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.getSignedJwtToken = async function () {
+  const jk = (process.env.JWT_EXPIRATION);
+  
+  return JWT.sign({ id: this._id }, process.env.JWT_SECRET as string, {
+    expiresIn: 7000,
+  });
+};
+
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
